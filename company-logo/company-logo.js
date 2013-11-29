5cc3e6534fa5de392b0f1bb723bd100caccc3d08
mateusz-shorelabs <m.ksiazek@shorelabs.com>
2013-11-29 13:17:32 +0100

    Added basic support for own company logo in top navigation

---
+++ company-logo/company-logo.js
@@ -0,0 +1,12 @@
+(function() {
+    var LOGO_URL, css;
+
+    LOGO_URL = "PNG-LOGO-ADDRESS";
+
+    css = "#content h2.brand { padding-top: 0; height: 35px; }#content h2.brand > a { float: left; margin-right: 5px; }.company-logo { max-width: 300px; max-height: 35px; }";
+
+    $('<style>').html(css).appendTo('head');
+
+    $('#content').find('h2.brand span').html('<img src="' + LOGO_URL + '" class="company-logo">');
+
+}).call(this);
