8abeae6d8f6e1dab18d4d09655768a30928bee16
mateusz-shorelabs <m.ksiazek@shorelabs.com>
2013-12-09 10:39:38 +0100

    Fixes for cct

--- color-card-transition/color-card-transition.js
+++ color-card-transition/color-card-transition.js
@@ -1,7 +1,11 @@
 (function() {
-    var checkTask, color, color_name, taskIterator, ws_id, _this = this;
+    var checkTask, color, color_name, isInvert, moveTask, taskIterator, ws_id,
+        _this = this;
+
     ws_id = null;
+
     color = null;
+
     color_name = null;
 
     $(window.board.model.attributes.workflow_stages).each(function(i, e) {
@@ -24,26 +28,42 @@
     };
 
     checkTask = function(task) {
-        var is_invert, updated_at;
+        var updated_at;
         if (task.model.attributes.workflow_stage_id === parseInt(ws_id) && !$(task.el).hasClass('cct')) {
             updated_at = moment(task.model.attributes.updated_at);
             if (moment().diff(updated_at, 'minutes') > 30) {
-                if ($.inArray(color_name, ['navy', 'green_dark', 'brown', 'gray_medium', 'gray_dark', 'black']) >= 0) {
-                    is_invert = true;
-                }
                 $(task.el).removeClass(task.model.attributes.card_color).addClass(color_name).addClass('cct');
-                if (is_invert) {
-                    return $(task.el).addClass('invert');
-                } else {
-                    return $(task.el).removeClass('invert');
-                }
+                return isInvert(task, color_name);
             }
+        }
+    };
+
+    moveTask = function(task) {
+        var task_attrs;
+        task_attrs = task.model.attributes;
+        if (task_attrs.workflow_stage_id !== parseInt(ws_id) && $(task.el).hasClass('cct')) {
+            $(task.el).removeClass(color_name).addClass(task_attrs.card_color).removeClass('cct');
+            return isInvert(task, task_attrs.card_color);
+        }
+    };
+
+    isInvert = function(task, color) {
+        var is_invert;
+        if ($.inArray(color, ['navy', 'green_dark', 'brown', 'gray_medium', 'gray_dark', 'black']) >= 0) {
+            is_invert = true;
+        }
+        if (is_invert) {
+            return $(task.el).addClass('invert');
+        } else {
+            return $(task.el).removeClass('invert');
         }
     };
 
     if (color && ws_id) {
         setTimeout(taskIterator, 500);
-        setInterval(taskIterator, 60000);
+        setTimeout(taskIterator, 2000);
+        setInterval(taskIterator, 50000);
+        window.board.on('task:render', moveTask);
     }
 
 }).call(this);
--- color-card-transition/src/color-card-transition.coffee
+++ color-card-transition/src/color-card-transition.coffee
@@ -8,21 +8,39 @@ $(window.board.model.attributes.card_types).each (i, e) =>
     color = e.id
     color_name = e.color_ref
 
+# Iterate all tasks on board
 taskIterator = () =>
   window.board.tasks.forEach (task) =>
     checkTask(task)
 
+# Check task
 checkTask = (task) =>
+  # If task is in selected for CCT column and isn't changed
   if task.model.attributes.workflow_stage_id == parseInt(ws_id) && !$(task.el).hasClass('cct')
     updated_at = moment(task.model.attributes.updated_at)
+    # Check that staying longer than 30 minutes
     if moment().diff(updated_at, 'minutes') > 30
-      is_invert = true if $.inArray(color_name, ['navy','green_dark','brown','gray_medium','gray_dark','black']) >= 0
       $(task.el).removeClass(task.model.attributes.card_color).addClass(color_name).addClass('cct')
-      if is_invert
-        $(task.el).addClass('invert')
-      else
-        $(task.el).removeClass('invert')
+      isInvert(task, color_name)
 
+# Back to base color
+moveTask = (task) =>
+  task_attrs = task.model.attributes
+  if task_attrs.workflow_stage_id != parseInt(ws_id) && $(task.el).hasClass('cct')
+    $(task.el).removeClass(color_name).addClass(task_attrs.card_color).removeClass('cct')
+    isInvert(task, task_attrs.card_color)
+
+# Check that color require invert class
+isInvert = (task, color) =>
+  is_invert = true if $.inArray(color, ['navy','green_dark','brown','gray_medium','gray_dark','black']) >= 0
+  if is_invert
+    $(task.el).addClass('invert')
+  else
+    $(task.el).removeClass('invert')
+
+# If column and color was selected
 if color && ws_id
   setTimeout(taskIterator, 500)
-  setInterval(taskIterator, 60000)
+  setTimeout(taskIterator, 2000)
+  setInterval(taskIterator, 50000)
+  window.board.on('task:render', moveTask);
